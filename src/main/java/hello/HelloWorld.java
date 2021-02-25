ad6156335365a0b2b0440fa0aa6e7d97205bdb47
kaun jovi <kaunjovi@gmail.com>
2021-02-25 21:50:55 +0530

    Just checking if the code push works

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -2,6 +2,7 @@ package hello;
 
 public class HelloWorld {
 
+	// This is the main method. 
 	public static void main(String[] args) {
 		System.out.println("Hello world. Will this get shown.");
 	}
